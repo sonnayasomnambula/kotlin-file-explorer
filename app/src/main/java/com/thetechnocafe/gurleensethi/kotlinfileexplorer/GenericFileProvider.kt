df45531610b9461e24a3067bfe1e7a12d931953c
glazomitsky <s.glazomitsky@mcsplus.ru>
2021-05-31 12:35:41 +0300

    remove GenericFileProvider class

--- app/src/main/java/com/thetechnocafe/gurleensethi/kotlinfileexplorer/GenericFileProvider.kt
+++
@@ -1,6 +0,0 @@
-package com.thetechnocafe.gurleensethi.kotlinfileexplorer
-
-import android.annotation.SuppressLint
-import android.support.v4.content.FileProvider
-
-class GenericFileProvider: FileProvider()
